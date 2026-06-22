847425c7926a4318b4781b27cf3d390e5320ffac
InfernalMindv2 <147970105+InfernalMindv2@users.noreply.github.com>
2026-06-22 23:01:33 +0300

    Sync Codeforces: 2146B - Merging the Sets [C++17 (GCC 7-32)]

---
+++ 2146B_Merging_the_Sets.cpp
@@ -0,0 +1,112 @@
+#include"bits/stdc++.h"
+#define print cout << "\n";
+#define ll long long int
+#define ull unsigned long long
+#define all(v) v.begin(),v.end()
+#define sz(v) ((ll)((v).size()))
+#define re(a) (a.resize(unique(a.begin(), a.end()) - a.begin()))
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace std;
+using namespace __gnu_pbds; 
+typedef tree<
+    pair<int,int>,
+    null_type,
+    less<pair<int,int>>,
+    rb_tree_tag,
+    tree_order_statistics_node_update>
+ordered_multiset;
+// SOONER OR LATER --> WE GOT EM' THE DREAM <33
+// (إِنَّ اللَّهَ وَمَلَائِكَتَهُ يُصَلُّونَ عَلَى النَّبِيِّ ۚ يَا أَيُّهَا الَّذِينَ آمَنُوا صَلُّوا عَلَيْهِ وَسَلِّمُوا تَسْلِيمًا)
+/*
+https://github.com/InfernalMindv2/CP/blob/main/tips
+https://github.com/InfernalMindv2/CP/blob/main/templatecpp
+*/
+ll gcdd(ll A, ll B)
+{
+	if(B==0)
+		return A;
+	else
+		return gcdd(B,A%B);
+}
+void Mind()
+{
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+ 
+}
+ll t1=1;
+const int oo =0x3f3f3f3f;
+int dx[] ={-1,0}
+,dy[] = {0,-1};
+ 
+ 
+void solve()
+{
+	ll n,m;
+	cin >> n >> m;
+	map<ll,ll>freq;
+	vector<vector<ll>>ar(n);
+	for(ll i=0;i<n;i++)
+	{
+		ll si;
+		cin >> si;
+		for(ll j=0;j<si;j++)
+		{
+			ll x;
+			cin >> x;
+			ar[i].push_back(x);
+			freq[x]++;
+		}
+	}
+	ll ans=1,two=0,more=0;
+	for(ll i=1;i<=m;i++)
+	{
+		if((freq[i])==0)
+		{
+			cout << "NO";
+			return;
+		}
+	}
+	for(ll i=0;i<n;i++)
+	{
+		map<ll,ll>pre;
+		for(ll j=0;j<ar[i].size();j++)
+			pre[ar[i][j]]++;
+		bool suc=1;
+		for(auto it: pre)
+		{
+			if(it.second>=freq[it.first])
+			{
+				suc=0;
+				break;
+			}
+		}
+		if(suc)
+			ans++;
+		if(ans>=3)
+		{
+			cout << "YES";
+			return;
+		}
+	}
+	cout << "NO";
+	
+	
+}
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    //Mind();
+    //prep();
+    //sieve();
+    //brute();
+    ll t=1;
+    cin >> t;
+    for(t1=1;t1<=t;t1++)
+    {
+    	solve();
+    	 //if(t>=1)
+       		 print;	
+    }
+}
