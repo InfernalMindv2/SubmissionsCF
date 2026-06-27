2a1b1189c6c0f05b517897e35f22c4a6540d5ce4
InfernalMindv2 <147970105+InfernalMindv2@users.noreply.github.com>
2026-06-27 17:48:20 +0300

    Sync Codeforces: 2240A - Another Popcount Problem [C++17 (GCC 7-32)]

---
+++ 2240A_Another_Popcount_Problem.cpp
@@ -0,0 +1,89 @@
+#include"bits/stdc++.h"
+#include<cstdint>
+#define print cout << "\n";
+#define ll long long int
+#define ull unsigned long long
+#define all(v) v.begin(),v.end()
+#define sz(v) ((ll)((v).size()))
+#define re(a) (a.resize(unique(a.begin(), a.end()) - a.begin()))
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace std;
+using namespace __gnu_pbds; 
+typedef tree<
+    pair<int,int>,
+    null_type,
+    less<pair<int,int>>,
+    rb_tree_tag,
+    tree_order_statistics_node_update>
+ordered_multiset;
+// SOONER OR LATER --> WE GOT EM' THE DREAM <33
+// (إِنَّ اللَّهَ وَمَلَائِكَتَهُ يُصَلُّونَ عَلَى النَّبِيِّ ۚ يَا أَيُّهَا الَّذِينَ آمَنُوا صَلُّوا عَلَيْهِ وَسَلِّمُوا تَسْلِيمًا)
+/*
+https://github.com/InfernalMindv2/CP/blob/main/tips
+https://github.com/InfernalMindv2/CP/blob/main/templatecpp
+*/
+ll gcdd(ll A, ll B)
+{
+	if(B==0)
+		return A;
+	else
+		return gcdd(B,A%B);
+}
+void Mind()
+{
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+ 
+}
+ 
+ll t1=1;
+const int oo =0x3f3f3f3f;
+int dx[] ={1,0}
+,dy[] = {0,1};
+int knightx[]={-2,-2,1,-1,1,-1,2,2}
+,knighty[]={1,-1,-2,-2,2,2,1,-1};
+const ll maa =1e16,mee=-4e16;
+ 
+void solve()
+{
+	ll n,k;
+	cin >> n >> k;
+	ll sum=0,ans=0;
+	for(ll i=0;i<32;i++)
+	{
+		ll cand=0,c=0;
+		for(ll j=1;j<=k;j++)
+		{
+			if((1LL<<(i))+sum<=n)
+			{
+				c++;
+				sum+=(1LL<<(i));
+			}
+			else
+				break;
+		}
+		//cout << sum << " " << ans;
+		//print;
+		ans+=c;
+	}
+	cout << ans;
+	
+}
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    //Mind();
+    //prep();
+    //sieve();
+    //brute();
+    ll t=1;
+    cin >> t;
+    
+    for(t1=1;t1<=t;t1++)
+    {
+    	solve();
+    	 //if(t1<t)
+       		 print;	
+    }
+}
