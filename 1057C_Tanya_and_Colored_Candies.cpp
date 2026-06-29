b43b4e50aa5d878dcac7cdded26500774ae34457
InfernalMindv2 <147970105+InfernalMindv2@users.noreply.github.com>
2026-06-29 15:44:57 +0300

    Sync Codeforces: 1057C - Tanya and Colored Candies [C++17 (GCC 7-32)]

---
+++ 1057C_Tanya_and_Colored_Candies.cpp
@@ -0,0 +1,127 @@
+#include"bits/stdc++.h"
+#include<cstdint>
+#define print cout << "\n";
+#define ll long long int
+#define ull unsigned long long
+#define all(v) v.begin(),v.end()
+#define sz(v) ((ll)((v).size()))
+#define re(a) (a.resize(unique(a.begin(), a.end()) - a.begin()))
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace std;
+using namespace __gnu_pbds; 
+typedef tree<
+    pair<int,int>,
+    null_type,
+    less<pair<int,int>>,
+    rb_tree_tag,
+    tree_order_statistics_node_update>
+ordered_multiset;
+// SOONER OR LATER --> WE GOT EM' THE DREAM <33
+// (إِنَّ اللَّهَ وَمَلَائِكَتَهُ يُصَلُّونَ عَلَى النَّبِيِّ ۚ يَا أَيُّهَا الَّذِينَ آمَنُوا صَلُّوا عَلَيْهِ وَسَلِّمُوا تَسْلِيمًا)
+/*
+https://github.com/InfernalMindv2/CP/blob/main/tips
+https://github.com/InfernalMindv2/CP/blob/main/templatecpp
+*/
+ll gcdd(ll A, ll B)
+{
+	if(B==0)
+		return A;
+	else
+		return gcdd(B,A%B);
+}
+ll fast_power(ll base, ll power) {
+    ll result = 1;
+    base = base % ((ll)998244353);
+    while(power > 0) {
+ 
+        if(power % 2 == 1) { 
+            result = ((result%((ll)998244353))*(base%((ll)998244353))) %((ll)998244353);
+        }
+        base = ((base%((ll)998244353)) * (base%((ll)998244353))) %((ll)998244353);
+        power = power / 2; 
+    }
+    return result;
+}
+void Mind()
+{
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+ 
+}
+ 
+ll t1=1;
+const int oo =0x3f3f3f3f;
+int dx[] ={1,0}
+,dy[] = {0,1};
+int knightx[]={-2,-2,1,-1,1,-1,2,2}
+,knighty[]={1,-1,-2,-2,2,2,1,-1};
+const ll maa =1e16,mee=-4e16;
+int n,s,k;
+string x;
+int ar[60];
+ 
+int dp[51][4000];
+ll calc(ll i , ll cost)
+{
+	if(i>n)
+	{
+		if(cost>=k)
+			return 0;
+		else
+			return INT_MAX;
+	}
+	if(cost>=k)
+		return 0;
+	
+	if(dp[i][cost]!=-1)
+		return dp[i][cost];
+	
+	ll c1=INT_MAX,c2=INT_MAX;
+	for(ll xx=1;xx<=n;xx++)
+	{
+		if(!cost || (ar[i]<ar[xx]&&x[i]!=x[xx]))
+		{
+			c1=min(c1,abs(xx-i)+calc(xx,cost+ar[xx]));
+			//c1=min(c1,abs(xx-i)+calc(xx,cost));
+		}
+	}
+	//cout << c1 << " " << c2 << " " << i;
+	//print;  
+	return dp[i][cost]=c1;
+}
+void solve()
+{
+	memset(dp,-1,sizeof(dp));
+	memset(ar,0,sizeof(ar));
+	cin >> n >> s >> k;
+	for(ll i=1;i<=n;i++)
+		cin >> ar[i];
+	string xx;
+	cin >> xx;
+	x='.'+xx;
+	
+	ll ans=calc(s,0);
+	if(ans>=INT_MAX)
+		cout << -1;
+	else
+		cout << ans;
+	
+}
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    //Mind();
+    //prep();
+    //sieve();
+    //brute();
+    ll t=1;
+    //cin >> t;
+    
+    for(t1=1;t1<=t;t1++)
+    {
+    	solve();
+    	 //if(t1<t)
+       		 print;	
+    }
+}
